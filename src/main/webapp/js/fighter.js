67601942bcc008dd8ff51e79163bf2819791613b
Joseph Hajduk <joseph.hajduk@gmail.com>
2012-11-01 04:16:27 -0600

    little stuff and scaling

--- src/main/webapp/js/fighter.js
+++ src/main/webapp/js/fighter.js
@@ -1,32 +1,99 @@
-function Fighter (name,skin,x,y) {
-    this.id = id;
-    this.skin = skin;
+function Fighter (name,fighter_def) {
+    this.name = name;
+    this.fighter_def = fighter_def;
 
-    this.x = x;
-    this.y = y;
+    this.x = 0;
+    this.y = 0;
 
-    this.currentAction = Actions[skin+"_idle"];
+    this.currentAction = this.fighter_def.idle;
+    this.defaultAction = this.fighter_def.idle;
 
-    this.lastRender = new Date();
+    this.lastRender = new Date().getTime();
+
 
     this.do_action = function(new_action) {
         this.currentAction = new_action;
         this.currentAction.reset();
     }
 
-    this.render = function(context) {
-        if(this.currentAction.isFinished) {
-            this.currentAction = Actions[skin+"_idle"];
-            this.currentAction.reset();
-        }
-
+    this.render = function(canvas,context) {
         // move
-        positionDelta = this.currentAction.get_position_delta(this.lastRender)
+        positionDelta = this.currentAction.get_position_delta(this.lastRender,new Date().getTime())
         this.x += positionDelta.x;
         this.y += positionDelta.y;
 
+        if(this.currentAction.isFinished()) {
+            this.currentAction = this.defaultAction;
+            this.currentAction.reset();
+        }
+
         // draw
-        this.currentAction.render(context,x,y)
+        this.currentAction.render(context,this.x,this.y)
+        this.currentAction.render(context,this.x-canvas.width,this.y)
+        this.currentAction.render(context,this.x+canvas.width,this.y)
+        this.lastRender = new Date().getTime();
+    }
+
+    // ACTIONS
+
+    this.stop = function() {
+        if(!this.currentAction.locking) {
+            this.currentAction = this.fighter_def.idle;
+            this.currentAction.reset();
+        }
+    }
+
+    this.dash_forward = function() {
+        if(this.currentAction != this.fighter_def.dash_forward && !this.currentAction.locking) {
+            this.currentAction = this.fighter_def.dash_forward;
+            this.currentAction.reset();
+        }
+    }
+
+    this.dash_backward = function() {
+        if(this.currentAction != this.fighter_def.dash_backward && !this.currentAction.locking){
+            this.currentAction = this.fighter_def.dash_backward;
+            this.currentAction.reset();
+        }
+    }
+
+    this.duck = function() {
+        if(this.currentAction != this.fighter_def.duck && this.currentAction != this.fighter_def.crouching && !this.currentAction.locking) {
+            this.currentAction = this.fighter_def.duck;
+            this.currentAction.reset();
+            this.defaultAction = this.fighter_def.crouching;
+        }
+    }
+
+    this.stand = function() {
+        // todo: reverse animation
+        this.defaultAction = this.fighter_def.idle;
+    }
+
+    this.jump_forward = function() {
+        if(!this.currentAction.locking){
+            this.currentAction = this.fighter_def.jump_forward;
+            this.currentAction.reset();
+        }
+    }
+
+    this.jump = function() {
+        //TODO: note works better to work this stuff out during the controls???
+        if(this.currentAction == this.fighter_def.dash_forward) {
+            this.jump_forward();
+        }
+
+        if(!this.currentAction.locking){
+            this.currentAction = this.fighter_def.jump_up;
+            this.currentAction.reset();
+        }
+    }
+
+    this.special1 = function() {
+        if(this.currentAction != this.fighter_def.special1 && !this.currentAction.locking){
+            this.currentAction = this.fighter_def.special1;
+            this.currentAction.reset();
+        }
     }
 }
 
--- src/main/webapp/js/test.js
+++ src/main/webapp/js/test.js
@@ -1,12 +1,14 @@
-var canvas = document.getElementById("main");
-var context = canvas.getContext("2d");
+var game_canvas = document.getElementById("main");
+var game_context = game_canvas.getContext("2d");
 
 sheet_ryu_idle.image_sheet.onload = function() {
                                       setInterval('draw_handler()', 75);
                                   };
 
 var my_fighter = new Fighter("ryu",ryu);
-my_fighter.y = 120;
+my_fighter.y = 160;
+
+var moving = 0;
 
 $(document).keydown(function(e){
 switch(e.keyCode) {
@@ -17,9 +19,13 @@ switch(e.keyCode) {
     case 68:
         //forwards
         my_fighter.dash_forward();
+        moving = 1;
         break;
     case 87:
-        my_fighter.jump();
+        if(moving == 1)
+            my_fighter.jump_forward();
+        else
+            my_fighter.jump();
         break;
     case 70:
         my_fighter.special1();
@@ -42,6 +48,7 @@ switch(e.keyCode) {
     case 68:
         //forwards
         my_fighter.stop();
+        moving = 0;
         break;
     case 83:
         my_fighter.stand();
@@ -51,9 +58,10 @@ switch(e.keyCode) {
 
 
 function draw_handler() {
-    canvas.width = canvas.width;
+    game_canvas.width = game_canvas.width;
 
-    my_fighter.x = my_fighter.x % canvas.width;
+    my_fighter.x = my_fighter.x % game_canvas.width;
 
-    my_fighter.render(canvas,context)
+    my_fighter.render(game_canvas,game_context)
+
 }
